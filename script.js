7333dcdba7b749dd89aec5417b64be898473f66e
lavanhien-test <lavanhien019250@gmail.com>
2024-10-24 19:12:24 +0700

    Upload bai 05

---
+++ script.js
@@ -0,0 +1,66 @@
+// var button = document.querySelector("button");
+// button.onclick = function () {
+//     alert("Hello World!");
+// }
+
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+// var hoTen = "La Van Hien";
+
+// // Noi chuoi
+// var d = c + " " + hoTen;
+// console.log(d);
+
+// var a = 20;
+// var b = a++ - --a + ++a;
+// console.log(b);
+
+// var a = 20;
+// var b = a++ - a-- + --a * 3;
+// console.log(b);
+
+// var a = 20;
+// var b = --a - ++a + --a - a++ + a-- * 2;
+// console.log(b);
+
+// var a = 20;
+// var b = --a - a++ + a++ - --a + a++ * 2;
+// console.log(b);
+
+// var a = 10;
+// var b = 10.5;
+// var c = Infinity;
+// var d = -Infinity;
+// var e = 10 / "a";
+// console.log(e);
+
+// var msg1 = 'Day la string dau dau nhay don';
+// var msg2 = "Day la string dau dau nhay kep";
+// var msg3 = `Day la string dau dau backtick`;
+// console.log(msg3);
+
+// var a;
+// console.log(a);
+
+// var a = null;
+// console.log(a); 
+
+// function tinhTong (a, b, c) {
+//     return a + b + c;
+// }
+
+// var x = tinhTong(10, 20, 30);
+// console.log(x);
+
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    job: "Developer"
+};
+
+console.log(person.firstName);
+console.log(person.age);
---
+++ script2.js
@@ -0,0 +1,180 @@
+// alert("Hello World");
+
+// var res = confirm("Ban da tren 18 tuoi?");
+// console.log(res);
+
+// var res = prompt("Nhap n: ");
+// console.log(res);
+// console.warn(res);
+// console.error(res);
+
+// setTimeout(function () {
+//     console.log("A");
+// }, 1000);
+
+// setInterval(function () {
+//     console.log("B");
+// }, 1000);
+
+// var b = setInterval(function () {
+//     console.log("B");
+// }, 1000);
+
+// setTimeout(function(){
+//     clearInterval(b);
+// }, 5000);
+
+// var a = 10;
+// var b = "Noi dung ...";
+// var c;
+// var d = null;
+// var e = true;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof (a + b));
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof a == "number");
+
+// var fullName = "La Van Hien"
+// console.log(fullName.length);
+
+// var myString = "Xin chao! Toi ten Hien. Toi nam nay 18 tuoi.";
+// console.log(myString.indexOf("Toi"));
+// console.log(myString.indexOf("Hien"));
+// console.log(myString.indexOf("hien"));
+// console.log(myString.indexOf("Nam"));
+// console.log(myString.indexOf("nam"));
+// console.log(myString.indexOf("Toi", 11));
+
+// var myString = "Xin chao! Toi ten Hien. Toi nam nay 18 tuoi.";
+// console.log(myString.slice(10, 20));
+// console.log(myString.slice(0, 8));
+// console.log(myString.slice(0, 9));
+// console.log(myString.slice(10, 22));
+// console.log(myString);
+
+// var myString = "Xin chao! Toi ten Hien. Toi nam nay 18 tuoi.";
+// console.log(myString.replace("Toi", "Minh"));
+// console.log(myString.replace(/Toi/g, "Minh"));
+
+// var myString = "La Van Hien";
+// console.log(myString.charAt());
+// console.log(myString.charAt(0));
+// console.log(myString.charAt(1));
+// console.log(myString.charAt(myString.length - 1));
+
+// var myString = `   HTML5,                  CSS3,  Javascript`;
+// myString = myString.replace(/^\s+|\s+$|\s(?=\s)/g, "");
+// console.log(myString);
+// console.log(myString.split());
+// console.log(myString.split(""));
+// console.log(myString.split(", "));
+// console.log(myString.split(", ", 2));
+// console.log(myString.split())
+
+// var a = 10;
+// var b = "Test";
+// var res = a / b;
+// console.log(res);
+// console.log(typeof res);
+// console.log(isNaN(res));
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.toString());
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.join());
+// console.log(list.join(""));
+// console.log(list.join(","));
+// console.log(list.join(", "));
+// console.log(list.join(" - "));
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.pop());
+// console.log(list);
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.push("Bootstrap 4", "ReactJS"));
+// console.log(list);
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.shift());
+// console.log(list);
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.unshift("Bootstrap 4", "ReactJS"));
+// console.log(list);
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.splice(2, 0, "Bootstrap 4", "ReactJS"));
+// console.log(list);
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.splice(1, 1));
+// console.log(list);
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list.splice(2, 1, "Bootstrap 4", "ReactJS"));
+// console.log(list);
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// var list2 = ["Bootstrap 4", "ReactJS"];
+// console.log(list.concat(list2));
+// console.log(list);
+// console.log(list2);
+
+// var list = ["HTML5", "CSS3", "Javascript", "Bootstrap 4", "ReactJS"];
+// console.log(list.slice(3));
+// console.log(list.slice(1, 3));
+// console.log(list.slice(-3, -1));
+// console.log(list);
+
+// var inforUser = {
+//     fullName: "La Van Hien",
+//     email: "test@gmail.com",
+//     phone: "0123456789"
+// }
+
+// for(key in inforUser){
+//     console.log(key + " " + inforUser[key]);
+// }
+
+// var list = ["HTML5", "CSS3", "Javascript"];
+// console.log(list);
+
+// for(var i = 0; i < list.length; i++){
+//     console.log(list[i]);
+// }
+
+// for(item of list){
+//     console.log(item);
+// }
+
+// var myString = "La Van Hien";
+
+// for(var i = 0; i < myString.length; i++){
+//     console.log(myString[i]);
+// }
+
+// for(item of myString){
+//     console.log(item);
+// }
+
+// PHAM VI KHAI BAO BIEN
+
+// var a = 10;
+// if(a == 10){
+//     var b = 20;
+//     console.log(b);
+// }
+// console.log(b);
+
+var a = 10;
+if(a == 10){
+    let b = 20;
+    console.log(b);
+}
+console.log(b);
